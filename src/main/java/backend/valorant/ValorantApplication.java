1db9f85d84d86ad253221907607cb3b33382d4e3
Yusuf Amir Faisal <yusufamirfaisal01@gmail.com>
2022-11-09 21:40:21 +0700

    first commit

---
+++ src/main/java/backend/valorant/ValorantApplication.java
@@ -0,0 +1,13 @@
+package backend.valorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/valorant/ValorantApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.valorant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
